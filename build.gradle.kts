3fdbb4c4819a52b310bce8858edd70a903300583
ASD781 <55333948+asdeshpande888@users.noreply.github.com>
2020-02-14 14:38:36 -0600

    Fixed logging error message and added test file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.3.0")
+    testImplementation("org.slf4j:slf4j-simple:1.7.26")
 }
 
 tasks {
@@ -20,4 +22,8 @@ tasks {
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
